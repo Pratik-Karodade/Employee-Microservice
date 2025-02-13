8f45404476d4c0b8de9f997ced9d2af291868bc2
Pratik-Karodade <karodadepratik@gmail.com>
2025-02-13 07:41:41 +0530

    Lombok and webclient issue resolved. Application is working fine using WebClient.

--- employee-service/employee-service/src/main/java/com/project/employeeservice/service/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/com/project/employeeservice/service/EmployeeServiceImpl.java
@@ -9,13 +9,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService{
 
     EmployeeRepository employeeRepository;
-    RestTemplate restTemplate;
+ //   RestTemplate restTemplate;
+    WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -40,10 +42,16 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public APIResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).get();
-        ResponseEntity<DepartmentDto> departmentDtoResponseEntity = restTemplate
-                .getForEntity("http://localhost:8080/api/departments/getDepartment/"+employee.getDepartmentCode(),
-                        DepartmentDto.class);
-        DepartmentDto departmentDto = departmentDtoResponseEntity.getBody();
+//        ResponseEntity<DepartmentDto> departmentDtoResponseEntity = restTemplate
+//                .getForEntity("http://localhost:8080/api/departments/getDepartment/"+employee.getDepartmentCode(),
+//                        DepartmentDto.class);
+//                        DepartmentDto departmentDto = departmentDtoResponseEntity.getBody();
+        DepartmentDto departmentDto = webClient.get().uri("http://localhost:8080/api/departments/getDepartment/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+
+
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
                 employee.getFirstName(),
